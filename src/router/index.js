9c6a6aa26b6b0b570e17074a9ddd221c6d7541c0
wangxinloo <wangxinloo@163.com>
2018-03-30 12:15:49 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/page/index'
+import Content from '@/page/content'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/content/:id',
+      name: 'Content',
+      component: Content
+    }
+  ]
+})
