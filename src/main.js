9e965a9e291dcd71cd654bc5664e7783a51c40b2
wangxinloo <wangxinloo@163.com>
2018-03-30 14:20:37 +0800

    1

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import api from './api/index.js'
 // 将API方法绑定到全局
 Vue.prototype.$api = api
 
+// 引用工具文件
+import utils from './utils/index.js'
+// 将工具方法绑定到全局
+Vue.prototype.$utils = utils
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>index page</div>
+  <div>
+  	<Header></Header>
+  	<div class="article_list">
+  		<ul>
+  			<li v-for="i in list">
+          <time v-text="$utils.goodTime(i.create_at)"></time>
+          <router-link :to="'/content/' + i.id">
+            {{ i.title }}
+          </router-link>
+        </li>
+  		</ul>
+  	</div>
+  	<Footer></Footer>
+  </div>
 </template>
 <script>
-
+import Header from '../components/header.vue'
+import Footer from '../components/footer.vue'
 export default {
+	components: { Header, Footer },
+  data () {
+    return {
+      list:[]
+    }
+  },
   created () {
-    this.$api.get('/topics', null, r => {
-      console.log(r)
-    })
+    this.getData()
+  },
+  methods:{
+    getData () {
+      /*this.$api.get('/topics', null, r => {
+        console.log(r)
+        this.list = r.data
+      })*/
+      var v = this
+      v.$api.get('topics',null,function(r){
+        v.list = r.data
+      })
+    }
   }
 }
 </script>
+<style>
+  .article_list {margin: auto;}
+</style>
